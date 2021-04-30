68b591a7500fbd5e6de5871f766cde1ea1f38b47
king-dragon <utkarshchauhan17@outlook.com>
2021-04-30 19:07:08 +0530

    create theme customizer

---
+++ includes/theme-customizer.php
@@ -0,0 +1,20 @@
+<?php
+
+function ju_customize_register($wp_customize)
+{
+    // echo '<pre>';
+    // var_dump( $wp_customize);
+    // echo '</pre>';
+
+
+    $wp_customize->get_section('title_tagline')->title      = 'General';
+
+
+    $wp_customize->add_panel('Test', [
+        'title'         =>  __('Test', 'test'),
+        'description'   =>  '<p>Test Theme Settings</p>',
+        'priority'      =>  160
+    ]);
+    ju_social_customizer_section($wp_customize);
+    ju_misc_customizer_section($wp_customize);
+}
